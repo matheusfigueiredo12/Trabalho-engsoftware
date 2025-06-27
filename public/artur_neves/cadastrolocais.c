61ba8139dba4c6c7dbca9f3b359e8f700c46612f
Artur Neves <arturfneves17@gmail.com>
2025-06-27 16:24:09 -0300

    finalizado cadastro de local

--- public/artur_neves/cadastrolocais.c
+++ public/artur_neves/cadastrolocais.c
@@ -8,13 +8,19 @@
 typedef struct {
     char nome[TAM_NOME_LOCAL];
 } Local;
+
 void cadastrarLocal(Local locais[], int *total) {
     if (*total >= MAX_LOCAIS) {
         printf("Limite de locais atingido!\n");
         return;
     }
 
-   
+    getchar();
+    printf("\n--- Cadastro de Local ---\n");
+    printf("Nome do local: ");
+    fgets(locais[*total].nome, TAM_NOME_LOCAL, stdin);
+    locais[*total].nome[strcspn(locais[*total].nome, "\n")] = '\0';
+
     for (int i = 0; i < *total; i++) {
         if (strcmp(locais[i].nome, locais[*total].nome) == 0) {
             printf("Local já cadastrado!\n");
@@ -36,4 +42,64 @@ void listarLocais(Local locais[], int total) {
     for (int i = 0; i < total; i++) {
         printf("%d. %s\n", i + 1, locais[i].nome);
     }
+}
+
+void excluirLocal(Local locais[], int *total) {
+    char nomeBusca[TAM_NOME_LOCAL];
+    getchar();
+    printf("Digite o nome do local a excluir: ");
+    fgets(nomeBusca, TAM_NOME_LOCAL, stdin);
+    nomeBusca[strcspn(nomeBusca, "\n")] = '\0';
+
+    for (int i = 0; i < *total; i++) {
+        if (strcmp(locais[i].nome, nomeBusca) == 0) {
+            for (int j = i; j < *total - 1; j++) {
+                locais[j] = locais[j + 1];
+            }
+            (*total)--;
+            printf("Local removido com sucesso!\n");
+            return;
+        }
+    }
+
+    printf("Local não encontrado.\n");
+}
+
+void menuLocais() {
+    Local locais[MAX_LOCAIS];
+    int totalLocais = 0;
+    int opcao;
+
+    do {
+        printf("\n=== MENU LOCAIS ===\n");
+        printf("1. Cadastrar local\n");
+        printf("2. Listar locais\n");
+        printf("3. Excluir local\n");
+        printf("0. Voltar\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                cadastrarLocal(locais, &totalLocais);
+                break;
+            case 2:
+                listarLocais(locais, totalLocais);
+                break;
+            case 3:
+                excluirLocal(locais, &totalLocais);
+                break;
+            case 0:
+                printf("Retornando ao menu principal...\n");
+                break;
+            default:
+                printf("Opção inválida.\n");
+        }
+
+    } while (opcao != 0);
+}
+
+int main() {
+    menuLocais();
+    return 0;
 }
